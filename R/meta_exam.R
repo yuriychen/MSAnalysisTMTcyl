8926366a6f81fd97774a391f987e9b79a7b257b2
yuriychen <ychen209@qq.com>
2021-01-20 16:38:41 +0800

    update

---
+++ R/meta_exam.R
@@ -0,0 +1,14 @@
+#' meta file example
+#'
+#' meta file example for a 6-plex TMT data set.
+#'
+#' @format A data frame with 18 rows and 6 variables:
+#' \describe{
+#'   \item{sample}{sample names}
+#'   \item{condition}{experiment conditions}
+#'   \item{repeat}{experiment repeats}
+#'   \item{reference}{1 for reference and NA for not}
+#'   \item{set}{experiment sets}
+#'   \item{channel}{experiment channels the MaxQuant reporter column names}
+#' }
+"meta_exam"
---
+++ R/prot_mean_sd_example.R
@@ -0,0 +1,18 @@
+#' TMT mean and sd data set
+#'
+#' TMT mean and sd data set by conditions.
+#'
+#' @format A data frame with 133 rows and 10 variables:
+#' \describe{
+#'   \item{mean_0hr}{mean of 0hr condition}
+#'   \item{sd_0hr}{sd of 0hr condition}
+#'   \item{mean_3hr}{mean of 3hr condition}
+#'   \item{sd_3hr}{sd of 3hr condition}
+#'   \item{mean_9hr}{mean of 9hr condition}
+#'   \item{sd_9hr}{sd of 9hr condition}
+#'   \item{mean_10.5hr}{mean of 10.5hr condition}
+#'   \item{sd_10.5hr}{sd of 10.5hr condition}
+#'   \item{mean_12hr}{mean of 12hr condition}
+#'   \item{sd_12hr}{sd of 12hr condition}
+#' }
+"prot_mean_sd_example"
